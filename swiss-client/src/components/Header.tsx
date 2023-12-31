9bb657cee2a1e5b58569ac5983c533cec69a8254
muarif <muariftahir961@gmail.com>
2023-12-31 15:27:28 +0500

    added project successfully

---
+++ swiss-client/src/components/Header.tsx
@@ -0,0 +1,158 @@
+
+'use client';
+
+import { Carousel } from 'flowbite-react';
+
+function Header() {
+    return (
+        <div style={{ overflow: 'hidden'}} className="hh">
+            <Carousel style={{ transition: ".3s linear all" }} slideInterval={3000}>
+                <div style={{ width: '100%', overflow: 'hidden',position:'relative' }}>
+                    <div  className='' style={{position:'absolute',top:'50%',left:'10%'}}>
+                        <h2 style={{fontWeight:'bolder',color:'rgb(255,253,208)',outline:"2px solid "}} className='sm:text-4xl md:text-6xl lg:text-7xl xl:text-7xl 2xl:text-7xl '>Malaysia</h2>
+
+                    </div>
+
+                    <img className='' style={{ width: '100%', }} src="https://images.unsplash.com/photo-1602427384420-71c70e2b2a2f?q=80&w=2006&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="..." />
+                </div>
+                <div style={{ width: '100%', overflow: 'hidden',position:'relative' }}>
+                <div className='' style={{position:'absolute',top:'50%',left:'10%'}}>
+                        <h2 style={{fontWeight:'bolder',color:'rgb(255,253,208)',outline:"2px solid "}} className='sm:text-4xl md:text-6xl lg:text-7xl xl:text-7xl 2xl:text-7xl '>Thailand</h2>
+
+                    </div>
+
+                    <img className='' style={{ width: '100%' }} src="https://images.unsplash.com/photo-1528181304800-259b08848526?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="..." />
+                </div>
+                <div style={{ width: '100%', overflow: 'hidden',position:'relative' }}>
+                <div className='' style={{position:'absolute',top:'50%',left:'10%'}}>
+                        <h2 style={{fontWeight:'bolder',color:'rgb(255,253,208)',outline:"2px solid "}} className='sm:text-4xl md:text-6xl lg:text-7xl xl:text-7xl 2xl:text-7xl '>Singapore</h2>
+
+                    </div>
+
+                    <img className='' style={{ width: '100%' }} src="https://images.unsplash.com/photo-1525625293386-3f8f99389edd?q=80&w=1952&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="..." />
+                </div>
+                <div style={{ width: '100%', overflow: 'hidden',position:'relative' }}>
+                <div className='' style={{position:'absolute',top:'50%',left:'10%'}}>
+                        <h2 style={{fontWeight:'bolder',color:'rgb(255,253,208)',outline:"2px solid "}} className='sm:text-4xl md:text-6xl lg:text-7xl xl:text-7xl 2xl:text-7xl '>Sri Lanka</h2>
+
+                    </div>
+
+                    <img className='' style={{ width: '100%' }} src="https://images.unsplash.com/photo-1580794749460-76f97b7180d8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="..." />
+                </div>
+                <div style={{ width: '100%', overflow: 'hidden',position:'relative' }}>
+                <div className='' style={{position:'absolute',top:'50%',left:'10%'}}>
+                        <h2 style={{fontWeight:'bolder',color:'rgb(255,253,208)',outline:"2px solid "}} className='sm:text-4xl md:text-6xl lg:text-7xl xl:text-7xl 2xl:text-7xl '>Cambodia</h2>
+
+                    </div>
+
+                    <img className='' style={{ width: '100%' }} src="https://images.unsplash.com/photo-1594444406267-35a7730c6867?q=80&w=1969&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="..." />
+                </div>
+                <div style={{ width: '100%', overflow: 'hidden',position:'relative' }}>
+                <div className='' style={{position:'absolute',top:'50%',left:'10%'}}>
+                        <h2 style={{fontWeight:'bolder',color:'rgb(255,253,208)',outline:"2px solid "}} className='sm:text-4xl md:text-6xl lg:text-7xl xl:text-7xl 2xl:text-7xl '>Indonesia</h2>
+
+                    </div>
+
+                    <img className='' style={{ width: '100%' }} src="https://plus.unsplash.com/premium_photo-1677829177642-30def98b0963?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="..." />
+                </div>
+                <div style={{ width: '100%', overflow: 'hidden',position:'relative' }}>
+                <div className='' style={{position:'absolute',top:'50%',left:'10%'}}>
+                        <h2 style={{fontWeight:'bolder',color:'rgb(255,253,208)',outline:"2px solid "}} className='sm:text-4xl md:text-6xl lg:text-7xl xl:text-7xl 2xl:text-7xl '>Vietnam</h2>
+
+                    </div>
+
+                    <img className='' style={{ width: '100%' }} src="https://images.unsplash.com/photo-1528127269322-539801943592?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="..." />
+                </div>
+                <div style={{ width: '100%', overflow: 'hidden',position:'relative' }}>
+                <div className='' style={{position:'absolute',top:'50%',left:'10%'}}>
+                        <h2 style={{fontWeight:'bolder',color:'rgb(255,253,208)',outline:"2px solid "}} className='sm:text-4xl md:text-6xl lg:text-7xl xl:text-7xl 2xl:text-7xl '>Dubai</h2>
+
+                    </div>
+
+                    <img className='' style={{ width: '100%', }} src="https://plus.unsplash.com/premium_photo-1661963885635-2b428cedce28?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="..." />
+                </div>
+                <div style={{ width: '100%', overflow: 'hidden',position:'relative' }}>
+                <div className='' style={{position:'absolute',top:'50%',left:'10%'}}>
+                        <h2 style={{fontWeight:'bolder',color:'rgb(255,253,208)',outline:"2px solid "}} className='sm:text-4xl md:text-6xl lg:text-7xl xl:text-7xl 2xl:text-7xl '>Saudi Arabia</h2>
+
+                    </div>
+
+                    <img className='' style={{ width: '100%' }} src="https://images.unsplash.com/photo-1506795213373-430e921fe2ed?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="..." />
+                </div>
+                <div style={{ width: '100%', overflow: 'hidden',position:'relative' }}>
+                <div className='' style={{position:'absolute',top:'50%',left:'10%'}}>
+                        <h2 style={{fontWeight:'bolder',color:'rgb(255,253,208)',outline:"2px solid "}} className='sm:text-4xl md:text-6xl lg:text-7xl xl:text-7xl 2xl:text-7xl '>Oman</h2>
+
+                    </div>
+
+                    <img className='' style={{ width: '100%' }} src="https://images.unsplash.com/photo-1504257538320-8a5d5f1c0f31?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="..." />
+                </div>
+                <div style={{ width: '100%', overflow: 'hidden',position:'relative' }}>
+                <div className='' style={{position:'absolute',top:'50%',left:'10%'}}>
+                        <h2 style={{fontWeight:'bolder',outline:"2px solid ",color:'rgb(255,253,208)'}} className='sm:text-4xl md:text-6xl lg:text-7xl xl:text-7xl 2xl:text-7xl '>Qatar</h2>
+
+                    </div>
+
+                    <img className='' style={{ width: '100%' }} src="https://images.unsplash.com/photo-1537345532964-7c8f0749f8b8?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="..." />
+                </div>
+                <div style={{ width: '100%', overflow: 'hidden',position:'relative' }}>
+                <div className='' style={{position:'absolute',top:'50%',left:'10%'}}>
+                        <h2 style={{fontWeight:'bolder',color:'rgb(255,253,208)',outline:"2px solid "}} className='sm:text-4xl md:text-6xl lg:text-7xl xl:text-7xl 2xl:text-7xl '>Egypt</h2>
+
+                    </div>
+
+                    <img className='' style={{ width: '100%' }} src="https://plus.unsplash.com/premium_photo-1661891622579-bee76e28c304?q=80&w=1781&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="..." />
+                </div>
+                <div style={{ width: '100%', overflow: 'hidden',position:'relative' }}>
+                <div className='' style={{position:'absolute',top:'50%',left:'10%'}}>
+                        <h2 style={{fontWeight:'bolder',color:'rgb(255,253,208)',outline:"2px solid "}} className='sm:text-4xl md:text-6xl lg:text-7xl xl:text-7xl 2xl:text-7xl '>Kazakhastan</h2>
+
+                    </div>
+
+                    <img className='' style={{ width: '100%' }} src="https://images.unsplash.com/photo-1582053403239-c58c07d86ce1?q=80&w=1931&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="..." />
+                </div>
+                <div style={{ width: '100%', overflow: 'hidden',position:'relative' }}>
+                <div className='' style={{position:'absolute',top:'50%',left:'10%'}}>
+                        <h2 style={{fontWeight:'bolder',color:'rgb(255,253,208)',outline:"2px solid "}} className='sm:text-4xl md:text-6xl lg:text-7xl xl:text-7xl 2xl:text-7xl '>Azerbaijan</h2>
+
+                    </div>
+
+                    <img className='' style={{ width: '100%' }} src="https://images.unsplash.com/photo-1596306499398-8d88944a5ec4?q=80&w=2012&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="..." />
+                </div>
+                <div style={{ width: '100%', overflow: 'hidden',position:'relative' }}>
+                <div className='' style={{position:'absolute',top:'50%',left:'10%'}}>
+                        <h2 style={{fontWeight:'bolder',color:'rgb(255,253,208)',outline:"2px solid "}} className='sm:text-4xl md:text-6xl lg:text-7xl xl:text-7xl 2xl:text-7xl '>China</h2>
+
+                    </div>
+
+                    <img className='' style={{ width: '100%' }} src="https://images.unsplash.com/photo-1506158669146-619067262a00?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="..." />
+                </div>
+                <div style={{ width: '100%', overflow: 'hidden',position:'relative' }}>
+                <div className='' style={{position:'absolute',top:'50%',left:'10%'}}>
+                        <h2 style={{fontWeight:'bolder',color:'rgb(255,253,208)',outline:"2px solid "}} className='sm:text-4xl md:text-6xl lg:text-7xl xl:text-7xl 2xl:text-7xl '>Hong Kong</h2>
+
+                    </div>
+
+                    <img className='' style={{ width: '100%' }} src="https://images.unsplash.com/photo-1518599807935-37015b9cefcb?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="..." />
+                </div>
+                <div style={{ width: '100%', overflow: 'hidden',position:'relative' }}>
+                <div className='' style={{position:'absolute',top:'50%',left:'10%'}}>
+                        <h2 style={{fontWeight:'bolder',color:'rgb(255,253,208)',outline:"2px solid "}} className='sm:text-4xl md:text-6xl lg:text-7xl xl:text-7xl 2xl:text-7xl '>Kenya</h2>
+
+                    </div>
+
+                    <img className='' style={{ width: '100%' }} src="https://plus.unsplash.com/premium_photo-1664304370557-233bccc0ac85?q=80&w=1779&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="..." />
+                </div>
+                <div style={{ width: '100%', overflow: 'hidden',position:'relative' }}>
+                <div className='' style={{position:'absolute',top:'50%',left:'10%'}}>
+                        <h2 style={{fontWeight:'bolder',color:'rgb(255,253,208)',outline:"2px solid "}} className='sm:text-4xl md:text-6xl lg:text-7xl xl:text-7xl 2xl:text-7xl '>Ethiopia</h2>
+
+                    </div>
+
+                    <img className='' style={{ width: '100%' }} src="https://images.unsplash.com/photo-1572888195250-3037a59d3578?q=80&w=1800&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="..." />
+                </div>
+            </Carousel>
+        </div>
+    );
+}
+export default Header
